5f17ce552160d6cfea2c59b9417168e7016eefc9
Administrator <https://github.com/15102981278/lc.git>
2020-03-04 18:01:53 +0800

    测试项目

---
+++ src/main/java/com/yinhe/dama/entity/User.java
@@ -0,0 +1,16 @@
+package com.yinhe.dama.entity;
+
+/**
+ * 用户类
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
